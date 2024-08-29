8b02a8e52dbe27dc7095c86f7b37c403bd34fba6
ikki_Arch <oye20000101@gmail.com>
2024-08-29 11:52:20 +0800

    [feat(yazi)] update

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,66 +1,165 @@
--- Border
-function Manager:render(area)
-	self.area = area
+-- Plugins
+require("full-border"):setup({
+	type = ui.Border.ROUNDED,
+})
+require("zoxide"):setup({
+	update_db = true,
+})
+require("session"):setup({
+	sync_yanked = true,
+})
 
-	local chunks = ui.Layout()
-		:direction(ui.Layout.HORIZONTAL)
-		:constraints({
-			ui.Constraint.Ratio(MANAGER.ratio.parent, MANAGER.ratio.all),
-			ui.Constraint.Ratio(MANAGER.ratio.current, MANAGER.ratio.all),
-			ui.Constraint.Ratio(MANAGER.ratio.preview, MANAGER.ratio.all),
-		})
-		:split(area)
+-- yatline
+local catppuccin_palette = {
+	rosewater = "#f4dbd6",
+	flamingo = "#f0c6c6",
+	pink = "#f5bde6",
+	mauve = "#c6a0f6",
+	red = "#ed8796",
+	maroon = "#ee99a0",
+	peach = "#f5a97f",
+	yellow = "#eed49f",
+	green = "#a6da95",
+	teal = "#8bd5ca",
+	sky = "#91d7e3",
+	sapphire = "#7dc4e4",
+	blue = "#8aadf4",
+	lavender = "#b7bdf8",
+	text = "#cad3f5",
+	subtext1 = "#b8c0e0",
+	subtext0 = "#a5adcb",
+	overlay2 = "#939ab7",
+	overlay1 = "#8087a2",
+	overlay0 = "#6e738d",
+	surface2 = "#5b6078",
+	surface1 = "#494d64",
+	surface0 = "#363a4f",
+	base = "#24273a",
+	mantle = "#1e2030",
+	crust = "#181926",
+}
 
-	local bar = function(c, x, y)
-		return ui.Bar(
-			ui.Rect({ x = math.max(0, x), y = math.max(0, y), w = math.min(1, area.w), h = math.min(1, area.h) }),
-			ui.Bar.TOP
-		):symbol(c)
-	end
+require("yatline"):setup({
+	section_separator = { open = "", close = "" },
+	inverse_separator = { open = "", close = "" },
+	part_separator = { open = "", close = "" },
 
-	return ya.flat({
-		-- Borders
-		ui.Border(area, ui.Border.ALL):type(ui.Border.ROUNDED),
-		ui.Bar(chunks[1], ui.Bar.RIGHT),
-		ui.Bar(chunks[3], ui.Bar.LEFT),
+	style_a = {
+		fg = catppuccin_palette.mantle,
+		bg_mode = {
+			normal = catppuccin_palette.blue,
+			select = catppuccin_palette.mauve,
+			un_set = catppuccin_palette.red,
+		},
+	},
+	style_b = { bg = catppuccin_palette.surface0, fg = catppuccin_palette.text },
+	style_c = { bg = catppuccin_palette.base, fg = catppuccin_palette.text },
 
-		bar("┬", chunks[1].right - 1, chunks[1].y),
-		bar("┴", chunks[1].right - 1, chunks[1].bottom - 1),
-		bar("┬", chunks[2].right, chunks[2].y),
-		bar("┴", chunks[2].right, chunks[1].bottom - 1),
+	permissions_t_fg = catppuccin_palette.green,
+	permissions_r_fg = catppuccin_palette.yellow,
+	permissions_w_fg = catppuccin_palette.red,
+	permissions_x_fg = catppuccin_palette.sky,
+	permissions_s_fg = catppuccin_palette.lavender,
 
-		-- Parent
-		Parent:render(chunks[1]:padding(ui.Padding.xy(1))),
-		-- Current
-		Current:render(chunks[2]:padding(ui.Padding.y(1))),
-		-- Preview
-		Preview:render(chunks[3]:padding(ui.Padding.xy(1))),
-	})
-end
+	selected = { icon = "󰻭", fg = catppuccin_palette.yellow },
+	copied = { icon = "", fg = catppuccin_palette.green },
+	cut = { icon = "", fg = catppuccin_palette.red },
 
--- Show user/group of files in status bar
-function Status:owner()
-	local h = cx.active.current.hovered
-	if h == nil or ya.target_family() ~= "unix" then
-		return ui.Line({})
-	end
+	total = { icon = "", fg = catppuccin_palette.yellow },
+	succ = { icon = "", fg = catppuccin_palette.green },
+	fail = { icon = "", fg = catppuccin_palette.red },
+	found = { icon = "", fg = catppuccin_palette.blue },
+	processed = { icon = "", fg = catppuccin_palette.green },
 
-	return ui.Line({
-		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
-		ui.Span(":"),
-		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
-		ui.Span(" "),
-	})
-end
+	tab_width = 20,
+	tab_use_inverse = true,
 
-function Status:render(area)
-	self.area = area
+	show_background = false,
 
-	local left = ui.Line({ self:mode(), self:size(), self:name() })
-	local right = ui.Line({ self:owner(), self:permissions(), self:percentage(), self:position() })
-	return {
-		ui.Paragraph(area, { left }),
-		ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
-		table.unpack(Progress:render(area, right:width())),
-	}
-end
+	display_header_line = true,
+	display_status_line = true,
+
+	header_line = {
+		left = {
+			section_a = {
+				{ type = "line", custom = false, name = "tabs", params = { "left" } },
+			},
+			section_b = {
+				{ type = "coloreds", custom = false, name = "githead" },
+			},
+			section_c = {},
+		},
+		right = {
+			section_a = {
+				{ type = "string", custom = false, name = "tab_path" },
+			},
+			section_b = {
+				{ type = "coloreds", custom = false, name = "task_workload" },
+			},
+			section_c = {
+				{ type = "coloreds", custom = false, name = "task_states" },
+			},
+		},
+	},
+
+	status_line = {
+		left = {
+			section_a = {
+				{ type = "string", custom = false, name = "tab_mode" },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "hovered_size" },
+			},
+			section_c = {
+				{ type = "string", custom = false, name = "hovered_name" },
+				{ type = "coloreds", custom = false, name = "count" },
+			},
+		},
+		right = {
+			section_a = {
+				{ type = "string", custom = false, name = "cursor_position" },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "cursor_percentage" },
+			},
+			section_c = {
+				{ type = "string", custom = false, name = "hovered_file_extension", params = { true } },
+				{ type = "coloreds", custom = false, name = "permissions" },
+			},
+		},
+	},
+})
+
+require("yatline-githead"):setup({
+	show_branch = true,
+	branch_prefix = "",
+	branch_symbol = "",
+	branch_borders = "",
+
+	commit_symbol = " ",
+
+	show_stashes = true,
+	stashes_symbol = " ",
+
+	show_state = true,
+	show_state_prefix = true,
+	state_symbol = "󱅉",
+
+	show_staged = true,
+	staged_symbol = " ",
+
+	show_unstaged = true,
+	unstaged_symbol = " ",
+
+	show_untracked = true,
+	untracked_symbol = " ",
+
+	prefix_color = catppuccin_palette.pink,
+	branch_color = catppuccin_palette.pink,
+	commit_color = catppuccin_palette.mauve,
+	stashes_color = catppuccin_palette.teal,
+	state_color = catppuccin_palette.lavender,
+	staged_color = catppuccin_palette.green,
+	unstaged_color = catppuccin_palette.yellow,
+	untracked_color = catppuccin_palette.pink,
+})
